041dad29e649ab1cef25dc5b6df44db2abdd3199
Marko Stamenkovic <marko.stamenkovic@cern.ch>
2022-05-03 07:32:25 +0200

    Updating reference files for the condor-run repo

---
+++ condor-run/job.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+DIRPATH=/afs/cern.ch/work/m/mstamenk/private/triple-h-mc-gen/scan-lambda-hhh/genproductions/bin/MadGraph5_aMCatNLO/
+
+
+cd $DIRPATH
+./gridpack_generation.sh GF_HHH_SM_c3_0_d4_0 ../../../GF_HHH_SM/GF_HHH_SM_c3_0_d4_0 
+
+
+
+
