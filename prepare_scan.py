3e5ad9f624d5903c2d4f8d0179234dae397f915a
Marko Stamenkovic <marko.stamenkovic@cern.ch>
2022-05-12 01:58:27 -0400

    Updatign

--- prepare_scan.py
+++ prepare_scan.py
@@ -24,15 +24,30 @@ path_to_ref = production + '/' + reference
 c3 = 1
 d4 = 1
 
-n_c3 = 11
-n_d4 = 11
+c3_min = 1.
+d4_min = 1.
 
-c3_scan = [ x for x in range(n_c3)]
-d4_scan = [ 10 * x for x in range(n_d4)]
+c3_max = 10.
+d4_max = 100.
+
+n_c3 = 50
+n_d4 = 50
+
+delta_c3 = (c3_max - c3_min)/ n_c3
+delta_d4 = (d4_max - d4_min)/ n_d4
+
+c3_scan = [ delta_c3 * x + c3_min for x in range(n_c3)]
+d4_scan = [ delta_d4 * x + d4_min for x in range(n_d4)]
+
+c3_scan_pre = [delta_c3 * x for x in range(6)]
+d4_scan_pre = [delta_d4 * x for x in range(6)]
+
+c3_scan = c3_scan_pre + c3_scan
+d4_scan = d4_scan_pre + d4_scan
 
 for c3 in c3_scan:
     for d4 in d4_scan:
-        new_scan = '%s_c3_%d_d4_%d'%(production,c3,d4)
+        new_scan = '%s_c3_%.3f_d4_%.3f'%(production,c3,d4)
         new_dir = production + '/' + new_scan
 
         # create dir
@@ -42,17 +57,20 @@ for c3 in c3_scan:
 
         # read file
         # start with customize
-        with open(path_to_ref + '/' + reference + customize_card) as f:
+        with open(path_to_ref + '/' + reference + customize_card, 'r') as f:
             inp = f.read() 
 
-        inp = inp.replace('set param_card tripcoup 4 0','set param_card tripcoup 4 %s'%c3)
-        inp = inp.replace('set param_card quartcoup 6 0','set param_card quartcoup 6 %s'%d4)
+        inp = inp.replace('set param_card tripcoup 4 0','set param_card tripcoup 4 %.3f'%c3)
+        inp = inp.replace('set param_card quartcoup 6 0','set param_card quartcoup 6 %.3f'%d4)
+
+        #print('set param_card tripcoup 4 %.3f'%c3)
+        #print('set param_card quartcoup 6 %.3f'%d4)
 
         with open(new_dir + '/' + new_scan + customize_card, 'w') as f:
             f.write(inp)
 
         # proc
-        with open(path_to_ref + '/' + reference + proc_card) as f:
+        with open(path_to_ref + '/' + reference + proc_card, 'r') as f:
             inp = f.read() 
 
         inp = inp.replace(reference, new_scan)
@@ -61,7 +79,7 @@ for c3 in c3_scan:
             f.write(inp)
 
         # run
-        with open(path_to_ref + '/' + reference + run_card) as f:
+        with open(path_to_ref + '/' + reference + run_card,'r') as f:
             inp = f.read() 
 
         inp = inp.replace(reference, new_scan)
@@ -70,7 +88,7 @@ for c3 in c3_scan:
             f.write(inp)
 
         # madspin
-        with open(path_to_ref + '/' + reference + madspin_card) as f:
+        with open(path_to_ref + '/' + reference + madspin_card, 'r') as f:
             inp = f.read() 
 
         inp = inp.replace(reference, new_scan)
